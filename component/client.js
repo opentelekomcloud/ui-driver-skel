5a94bdcd1580ac102593bc94c1f54423608e6447
Anton Kachurin <anton.kachurin@t-systems.com>
2020-09-08 16:34:55 +0300

    OTC machine-driver UI initial implementation (#10)

---
+++ component/client.js
@@ -0,0 +1,346 @@
+const appjson = 'application/json;charset=utf-8'
+const authURL = 'https://iam.eu-de.otc.t-systems.com/v3'
+
+const defaultDNS = ['100.125.4.25', '8.8.8.8']
+
+const resolve = Ember.RSVP.resolve
+const reject = Ember.RSVP.reject
+
+/**
+ * Select endpoint for current region for service
+ * @param service - object representing endpoint in token
+ * @param {string} region
+ */
+function withRegion(service, region) {
+  const endpoint = service.endpoints.find(ep => ep.region === region)
+  return endpoint.url
+}
+
+
+/**
+ * Convert external URL to rancher meta proxy's URL
+ * @param {string} url
+ * @returns {string}
+ */
+function viaProxy(url) {
+  url = url.replace('://', ':/')
+  return `/meta/proxy/${url}`
+}
+
+const validImageProperties = {
+  visibility:    'public',
+  protected:     true,
+  __support_kvm: true,
+  __os_type:     'Linux',
+  __os_bit:      64,
+}
+
+function authData(username, password, domainName, projectName) {
+  const data = {
+    "auth": {
+      "identity": {
+        "methods":  [
+          "password"
+        ],
+        "password": {
+          "user": {
+            "name":     username,
+            "password": password,
+            "domain":   {
+              "name": domainName,
+            }
+          }
+        }
+      }
+    }
+  }
+
+  if (projectName) {
+    data['auth']['scope'] = {
+      "project": {
+        "name": projectName,
+      }
+    }
+  } else {
+    data['auth']['scope'] = {
+      "domain": {
+        "name": domainName,
+      }
+    }
+  }
+
+  return data
+}
+
+/**
+ * @typedef OpenTelekomCloudClient
+ * @type {object}
+ * @property {string} vpcEndpoint - URL of VPCv1 endpoint
+ * @property {string} novaEndpoint - URL of compute v2 (Nova) endpoint
+ * @property {object} commonHeaders - set of commonly used headers
+ * @method {authFunction} authenticate - Authenticate client using credentials
+ */
+
+/**
+ * Create new client object
+ * @param region
+ * @return {OpenTelekomCloudClient}
+ */
+function otcClient(region) {
+  return {
+    vpcEndpoint:    '',
+    novaEndpoint:   '',
+    glanceEndpoint: '',
+
+    commonHeaders: {
+      accepts:     appjson,
+      contentType: appjson,
+    },
+
+    /**
+     * Authenticate client setting 'X-Auth-Token' header
+     * @param {string} username
+     * @param {string} password
+     * @param {string} domainName
+     * @param {string} projectName
+     */
+    authenticate(username, password, domainName, projectName) {
+      const json = JSON.stringify(authData(username, password, domainName, projectName))
+      console.log('Authorizing client: ' + json)
+
+      return $.post({
+        url:         `${viaProxy(authURL)}/auth/tokens`,
+        contentType: appjson,
+        data:        json,
+      }).then((response, _, jqXHR) => {
+        const token = response.token
+        console.log('Received token', JSON.stringify(token))
+        this.commonHeaders['X-Auth-Token'] = jqXHR.getResponseHeader('x-subject-token')
+        if (token.catalog === undefined) {
+          console.log('No service catalog provided')
+          return resolve()
+        }
+        // fill endpoints
+        token.catalog.forEach((srv) => {
+          switch (srv.name) {
+            case 'nova':
+              console.log('nova: ', JSON.stringify(srv))
+              this.novaEndpoint = viaProxy(withRegion(srv, region))
+              break
+            case 'vpc':
+              console.log('vpc: ', JSON.stringify(srv))
+              this.vpcEndpoint = viaProxy(withRegion(srv, region))
+              break
+            case 'glance':
+              console.log('ims: ', JSON.stringify(srv))
+              this.glanceEndpoint = viaProxy(withRegion(srv, region))
+              break
+          }
+        })
+        return resolve()
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    listNodeFlavors() {
+      return $.get({
+        headers: this.commonHeaders,
+        url:     this.novaEndpoint + '/flavors'
+      }).then(body => {
+        return resolve(body.flavors)
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    listNodeImages() {
+      return $.get({
+        headers: this.commonHeaders,
+        url:     this.glanceEndpoint + '/v2/cloudimages',
+        data:    validImageProperties,
+      }).then(body => {
+        return resolve(body.images)
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    /**
+     * Find VPC for project
+     * @return {Promise<Object[]>} list of VPC objects
+     */
+    listVPCs() {
+      return $.get({
+        headers: this.commonHeaders,
+        url:     this.vpcEndpoint + '/vpcs',
+      }).then(response => {
+        return resolve(response.vpcs)
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    /**
+     * Find subnets for given VPC
+     * @param {string} vpcID
+     * @return {Promise<object[]>} list of subnet objects
+     */
+    listSubnets(vpcID) {
+      return $.get({
+        headers: this.commonHeaders,
+        url:     `${this.vpcEndpoint}/subnets?vpc_id=${vpcID}`,
+      }).then(response => {
+        return resolve(response.subnets)
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    /**
+     * Wait until VPC is available
+     * @param {string} vpcID
+     * @param endTime
+     * @return {Promise<void>}
+     */
+    waitForVPCUp(vpcID, endTime) {
+      if (!endTime) {
+        endTime = new Date().getTime() + 10 * 1000
+      }
+      if (new Date().getTime() > endTime) {
+        return reject(new Error('Timeout reached'))
+      }
+      return $.get({
+        headers: this.commonHeaders,
+        url:     `${this.vpcEndpoint}/vpcs/${vpcID}`,
+      }).then(data => {
+        if (data.vpc.status === 'OK') {
+          return resolve()
+        }
+        return new Promise(resolve => setTimeout(() => resolve(this.waitForVPCUp(vpcID, endTime)), 1000))
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+
+    },
+
+    /**
+     * Create new VPC and wait until it's available
+     * @param {string} name
+     * @param {string} cidr
+     * @return {Promise<string>} VPC ID
+     */
+    createVPC(name, cidr) {
+      const data = {
+        vpc: {
+          name: name,
+          cidr: cidr
+        }
+      }
+      return $.post({
+        headers: this.commonHeaders,
+        url:     `${this.vpcEndpoint}/vpcs`,
+        data:    JSON.stringify(data)
+      }).then(data => {
+        const vpcID = data.vpc.id
+        return this.waitForVPCUp(vpcID).then(() => resolve(vpcID)).catch((e) => reject(e))
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    /**
+     * Wait until subnet in status ACTIVE
+     * @param {string} subnetID
+     * @param endTime
+     * @return {Promise<void>}
+     */
+    waitForSubnetUp(subnetID, endTime) {
+      if (!endTime) {
+        endTime = new Date().getTime() + 10 * 1000
+      }
+      if (new Date().getTime() > endTime) {
+        return reject(new Error('Timeout reached'))
+      }
+      return $.get({
+        headers: this.commonHeaders,
+        url:     `${this.vpcEndpoint}/subnets/${subnetID}`
+      }).then(data => {
+        if (data.subnet.status === 'ACTIVE') {
+          console.log('Subnet is active')
+          return
+        }
+        return new Promise(resolve => setTimeout(() => resolve(this.waitForSubnetUp(subnetID, endTime)), 1000))
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    /**
+     * Create new subnet instance
+     * @param {string} vpcID
+     * @param {string} name
+     * @param {string} cidr
+     * @param {string} gatewayIP
+     * @return {Promise<string>} subnet ID
+     */
+    createSubnet(vpcID, name, cidr, gatewayIP) {
+      const data = {
+        subnet: {
+          name:       name,
+          cidr:       cidr,
+          gateway_ip: gatewayIP,
+          vpc_id:     vpcID,
+          dnsList:    defaultDNS
+        }
+      }
+      return $.post({
+        headers: this.commonHeaders,
+        url:     `${this.vpcEndpoint}/subnets`,
+        data:    JSON.stringify(data)
+      }).then(data => {
+        const subnetID = data.subnet.id
+        return this.waitForSubnetUp(subnetID).then(() => resolve(subnetID)).catch((e) => reject(e))
+      }).catch(e => {
+        return reject(e.responseText)
+      })
+    },
+
+    /**
+     * List existing key pairs
+     * @return {Promise<Object[]>} key pairs
+     */
+    listKeyPairs() {
+      return $.get({
+        headers: this.commonHeaders,
+        url:     `${this.novaEndpoint}/os-keypairs`,
+      }).then(data => {
+        return resolve(data.keypairs)
+      }).catch(e => {
+        return reject(e)
+      })
+    },
+
+    listSecurityGroups() {
+      return $.get({
+        headers: this.commonHeaders,
+        url:     `${this.vpcEndpoint}/security-groups`,
+      }).then(data => {
+        return resolve(data.security_groups)
+      }).catch(e => {
+        return reject(e)
+      })
+    },
+
+    listProjects() {
+      return $.get({
+        headers: this.commonHeaders,
+        url:     `${viaProxy(authURL)}/auth/projects`
+      }).then(data => {
+        return resolve(data.projects)
+      }).catch(e => {
+        return reject(e)
+      })
+    },
+  }
+}
--- component/component.js
+++ component/component.js
@@ -1,83 +1,455 @@
 /*!!!!!!!!!!!Do not change anything between here (the DRIVERNAME placeholder will be automatically replaced at buildtime)!!!!!!!!!!!*/
-import NodeDriver from 'shared/mixins/node-driver';
-
+import NodeDriver from 'shared/mixins/node-driver'
 // do not remove LAYOUT, it is replaced at build time with a base64 representation of the template of the hbs template
-// we do this to avoid converting template to a js file that returns a string and the cors issues that would come along with that
+// we do this to avoid converting template to a js file that returns a string and the cors issues that would come along
+// with that
 const LAYOUT;
 /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
 
-
 /*!!!!!!!!!!!GLOBAL CONST START!!!!!!!!!!!*/
-// EMBER API Access - if you need access to any of the Ember API's add them here in the same manner rather then import them via modules, since the dependencies exist in rancher we dont want to expor the modules in the amd def
-const computed     = Ember.computed;
-const get          = Ember.get;
-const set          = Ember.set;
-const alias        = Ember.computed.alias;
-const service      = Ember.inject.service;
+// EMBER API Access - if you need access to any of the Ember API's add them here in the same manner rather then import
+// them via modules, since the dependencies exist in rancher we dont want to export the modules in the amd def
+const $ = Ember.$
+const computed = Ember.computed
+const observer = Ember.observer
+const get = Ember.get
+const set = Ember.set
+const alias = Ember.computed.alias
+const service = Ember.inject.service
+const reject = Ember.RSVP.reject
+const resolve = Ember.RSVP.resolve
+const all = Ember.RSVP.all
+const setProperties = Ember.setProperties
 
-const defaultRadix = 10;
-const defaultBase  = 1024;
+const defaultRadix = 10
+const defaultBase = 1024
 /*!!!!!!!!!!!GLOBAL CONST END!!!!!!!!!!!*/
 
+const diskTypes = ['SATA', 'SAS', 'SSD']
 
+const availabilityZones = [
+  'eu-de-01',
+  'eu-de-02',
+  'eu-de-03',
+]
 
-/*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
+const ubuntuRegex = new RegExp('_[Uu]buntu_')
+
+/**
+ * Convert string array to field array
+ * @param src {string[]}
+ * @returns {{label, value}[]}
+ */
+function a2f(src) {
+  return src.map((v) => ({ label: v, value: v }))
+}
+
+const languages = {
+  "en-us": {
+    "machine": {
+      "driverOtc": {
+        "access":              "User Credentials",
+        "authorize":           "Authorize",
+        "next":                {
+          "availability": "Next: Select Availability",
+          "networks":     "Next: Select Subnet",
+          "instance":     "Next: Select Instance Options"
+        },
+        "errors":              {
+          "akId":    "A Access Key ID is required",
+          "akS":     "A Access Key Secret is required",
+          "project": "A Project ID is required"
+        },
+        "loadingAvailability": "Loading Availability and VPCs",
+        "loadingNetworks":     "Loading Networks",
+        "loadingFlavors":      "Loading Instance Options",
+        "provided":            {
+          "password": "Password Provided"
+        },
+        "projectId":           "Project ID",
+        "osUsername":          "Username",
+        "osPassword":          "Password",
+        "osUserDomainName":    "Domain Name",
+        "osProjectName":       "Project Name",
+        "availableZone":       {
+          "header":   "Availability",
+          "label":    {
+            "zone":   "Availability Zone",
+            "region": "Region"
+          },
+          "dropdown": {
+            "zone": "Select a Zone"
+          }
+        },
+        "region":              null,
+        "vpcAndSecurity":      {
+          "header":   "Subnet and Security Groups",
+          "vpc":      "VPC",
+          "security": "Security Groups",
+          "subnet":   "Subnet",
+          "dropdown": {
+            "vpc":      "Select a VPC",
+            "security": "Select a Security Group",
+            "subnet":   "Select a Subnet"
+          }
+        },
+        "instance":            {
+          "header":         "Instance",
+          "options":        "Instance Options",
+          "flavorid":       "Flavor ID",
+          "images":         "Images",
+          "sshUser":        "SSH User",
+          "rootPassword":   "Root Password",
+          "privateKey":     "SSH Private Key",
+          "rootVolume":     "Root Volume Size",
+          "rootVolumeType": "Root Volume Type",
+          "size":           "Bandwidth Size",
+          "dropdown":       {
+            "flavorid": "Select a Flavor",
+            "imageid":  "Select a Image",
+            "type":     "Select a Volume Type"
+          }
+        }
+      }
+    }
+  }
+}
+
+const region = 'eu-de'
+
 export default Ember.Component.extend(NodeDriver, {
   driverName: '%%DRIVERNAME%%',
   config:     alias('model.%%DRIVERNAME%%Config'),
   app:        service(),
 
+  catalogUrls:  null,
+  step:         1,
+  _prevStep:    1,
+  errors:       [],
+  intl:         service(),
+  volumeTypes:  a2f(diskTypes),
+  itemsLoading: false,
+  flavors:      [],
+  images:       [],
+  vpcs:         [],
+  subnet:       null,
+
+  authSuccess:  false,
+  subnets:      [],
+  vpcEndpoint:  '',
+  novaEndpoint: '',
+  newVPC:       { create: false, name: '', cidr: '192.168.0.0/16' },
+  newSubnet:    { create: false, name: '', cidr: '192.168.0.0/24', gatewayIP: '192.168.0.1' },
+
+  otc: null,
+
   init() {
     // This does on the fly template compiling, if you mess with this :cry:
-    const decodedLayout = window.atob(LAYOUT);
-    const template      = Ember.HTMLBars.compile(decodedLayout, {
+    const decodedLayout = window.atob(LAYOUT)
+    const template = Ember.HTMLBars.compile(decodedLayout, {
       moduleName: 'nodes/components/driver-%%DRIVERNAME%%/template'
-    });
-    set(this,'layout', template);
+    })
+    set(this, 'layout', template)
 
-    this._super(...arguments);
+    this._super(...arguments)
 
   },
   /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
 
+
   // Write your component here, starting with setting 'model' to a machine with your config populated
-  bootstrap: function() {
-    // bootstrap is called by rancher ui on 'init', you're better off doing your setup here rather then the init function to ensure everything is setup correctly
+  bootstrap: function () {
     let config = get(this, 'globalStore').createRecord({
-      type: '%%DRIVERNAME%%Config',
-      cpuCount: 2,
-      memorySize: 2048,
-    });
+      type:             '%%DRIVERNAME%%Config',
+      region:           'eu-de',
+      username:         '',
+      password:         '',
+      domainName:       '',
+      projectName:      'eu-de',
+      availabilityZone: '',
+      vpcId:            '',
+      subnetId:         '',
+      flavorId:         '',
+      imageId:          '',
+      secGroups:        [],
+      k8sGroup:         true,
+      keypairName:      '',
+    })
 
-    set(this, 'model.%%DRIVERNAME%%Config', config);
+    set(this, 'config', config)
+    set(this, 'model.%%DRIVERNAME%%Config', config)
+
+    const lang = get(this, 'session.language') || 'en-us'
+    get(this, 'intl.locale')
+    this.loadLanguage(lang)
+
+    set(this, 'otc', otcClient(region))
+
+    console.log(`Config: ${JSON.stringify(config)}`)
   },
 
-  // Add custom validation beyond what can be done from the config API schema
+  actions: {
+    authClient() {
+      return get(this, 'otc').authenticate(
+        get(this, 'config.username'),
+        get(this, 'config.password'),
+        get(this, 'config.domainName'),
+        get(this, 'config.projectName'),
+      ).then(() => {
+        set(this, 'authSuccess', true)
+        set(this, 'errors', [])
+        set(this, 'step', 2)
+        return resolve()
+      }).catch(e => {
+        set(this, 'errors', [e])
+        return reject()
+      })
+    },
+
+    goToStep3() {
+      set(this, 'errors', [])
+      set(this, 'step', 3)
+    },
+
+    multiSecurityGroupSelect() {
+      let options = Array.prototype.slice.call($('.existing-security-groups')[0], 0);
+      let selectedOptions = [];
+
+      options.filterBy('selected', true).forEach((cap) => {
+        return selectedOptions.push(cap.value);
+      });
+      console.debug(`Selected security groups: ${selectedOptions}`)
+      set(this, 'config.secGroups', selectedOptions);
+    },
+
+    goToStep4() {
+      set(this, 'errors', [])
+      set(this, 'step', 4)
+    },
+
+  },
+
   validate() {
-    // Get generic API validation errors
-    this._super();
-    var errors = get(this, 'errors')||[];
-    if ( !get(this, 'model.name') ) {
-      errors.push('Name is required');
+    this._super(...arguments)
+    const errors = []
+
+    if (!get(this, 'config.flavorId')) {
+      errors.push('Flavor is required')
     }
 
-    // Add more specific errors
-
-    // Check something and add an error entry if it fails:
-    if ( parseInt(get(this, 'config.memorySize'), defaultRadix) < defaultBase ) {
-      errors.push('Memory Size must be at least 1024 MB');
+    if (!get(this, 'config.imageId')) {
+      errors.push('Image is required')
     }
 
-    // Set the array of errors for display,
-    // and return true if saving should continue.
-    if ( get(errors, 'length') ) {
-      set(this, 'errors', errors);
-      return false;
-    } else {
-      set(this, 'errors', null);
-      return true;
-    }
+    set(this, 'errors', errors)
+    return errors.length === 0
   },
 
-  // Any computed properties or custom logic can go here
-});
+  createVPC(cb) {
+    return get(this, 'otc').createVPC(
+      get(this, 'newVPC.name'),
+      get(this, 'newVPC.cidr'),
+    ).then(vpcID => {
+      set(this, 'config.vpcId', vpcID)
+      set(this, 'newVPC.create', false)
+      set(this, 'newVPC.name', '')
+      set(this, 'errors', [])
+      cb(true)
+    }).catch((er) => {
+      set(this, 'newVPC.name', '')
+      set(this, 'errors', [JSON.stringify(er)])
+      cb(false)
+    })
+  },
+  createSubnet(cb) {
+    return get(this, 'otc').createSubnet(
+      get(this, 'config.vpcId'),
+      get(this, 'newSubnet.name'),
+      get(this, 'newSubnet.cidr'),
+      get(this, 'newSubnet.gatewayIP'),
+    ).then(subnetID => {
+      set(this, 'config.subnetId', subnetID)
+      set(this, 'newSubnet.name', '')
+      set(this, 'newSubnet.create', false)
+      set(this, 'errors', [])
+      this.updateSubnets()
+      cb(true)
+    }).catch((er) => {
+      set(this, 'newSubnet.name', '')
+      set(this, 'errors', [JSON.stringify(er)])
+      cb(false)
+    })
+  },
+
+  authFieldsMissing:  true,
+  onAuthFieldsChange: observer('config.username', 'config.password', 'config.domainName', 'config.projectName', function () {
+    const missing = !(
+      get(this, 'config.username') &&
+      get(this, 'config.password') &&
+      get(this, 'config.domainName') &&
+      get(this, 'config.projectName')
+    )
+    set(this, 'authFieldsMissing', missing)
+  }),
+
+  projectChoices:       [],
+  projectChoicesUpdate: observer('config.username', 'config.password', 'config.domainName', function () {
+    if (!(
+      get(this, 'config.username') &&
+      get(this, 'config.password') &&
+      get(this, 'config.domainName')
+    )) {
+      return []
+    }
+    return this.otc.authenticate(
+      get(this, 'config.username'),
+      get(this, 'config.password'),
+      get(this, 'config.domainName'),
+      ''
+    ).then(() => {
+      return this.otc.listProjects().then(projects => {
+        const projectCh = projects.map(p => ({ label: p.name, value: p.name }))
+        set(this, 'projectChoices', projectCh)
+      })
+    })
+  }),
+
+  loadLanguage(lang) {
+    const translation = languages[lang]
+    const intl = get(this, 'intl')
+
+    intl.addTranslations(lang, translation)
+    intl.translationsFor(lang)
+    console.log('Added translation for ' + lang)
+  },
+
+  vpcChoices: computed('vpcs', function () {
+    const vpcs = get(this, 'vpcs')
+    return vpcs.map((vpc) => ({ label: `${vpc.name} (${vpc.id})`, value: vpc.id }))
+  }),
+  updateVPCs: function () {
+    return this.otc.listVPCs().then(vpcs => {
+      set(this, 'vpcs', vpcs)
+      console.log(`VPCs: ${JSON.stringify(vpcs)}`)
+      return resolve()
+    }).catch((e) => {
+      console.error(`Failed to get VPCs: ${e}`)
+      return reject()
+    })
+  },
+  loadVPCs:   observer('authSuccess', function () {
+    if (get(this, 'authSuccess')) {
+      this.updateVPCs()
+    }
+  }),
+
+  subnetChoices: computed('subnets', function () {
+    const subnets = get(this, 'subnets')
+    return subnets.map((sn) => ({ label: `${sn.name}(${sn.cidr})`, value: sn.id }))
+  }),
+  updateSubnets: function () {
+    const vpcId = get(this, 'config.vpcId')
+    if (!vpcId) {
+      return []
+    }
+    return get(this, 'otc').listSubnets(vpcId).then(subnets => {
+      console.log('Subnets: ', subnets)
+      set(this, 'subnets', subnets)
+      return resolve()
+    }).catch((e) => {
+      console.error('Failed to get subnets: ', e)
+      return reject()
+    })
+  },
+  vpcUpdated:    observer('config.vpcId', function () {
+    console.log(`VPC is now set to ${get(this, 'config.vpcId')}`)
+    this.updateSubnets()
+  }),
+
+  nodeFlavorChoices: computed('authSuccess', function () {
+    return this.otc.listNodeFlavors().then(flavors => {
+      console.log('Flavors: ', flavors)
+      return flavors.map((f) => ({ label: f.name, value: f.id }))
+    }).catch(() => {
+      console.log('Failed to load node flavors')
+    })
+  }),
+
+  azChoices: a2f(availabilityZones),
+
+  imageChoices: computed('authSuccess', function () {
+    if (!get(this, 'authSuccess')) {
+      return []
+    }
+    return this.otc.listNodeImages().then(images => {
+      return images.map(i => {
+        return {
+          label: i.name,
+          value: i.id,
+        }
+      })
+    })
+  }),
+
+  sgChoices: [],
+  sgUpdate:  observer('authSuccess', function () {
+    if (!get(this, 'authSuccess')) {
+      return
+    }
+    return this.otc.listSecurityGroups().then(groups => {
+      console.log(`Got groups: ${JSON.stringify(groups)}`)
+      const choices = groups.map(g => {
+        return {
+          label: `${g.name} (${g.id})`,
+          value: g.id
+        }
+      })
+      set(this, 'sgChoices', choices)
+    }).catch(() => {
+      console.log('Failed to load sec groups')
+      return reject()
+    })
+  }),
+
+  languageChanged: observer('intl.locale', function () {
+    const lang = get(this, 'intl.locale')
+    if (lang) {
+      this.loadLanguage(lang[0])
+    }
+  }),
+
+  readyForStep4: computed('config.secGroups', 'config.subnetId', function () {
+    return get(this, 'config.secGroups').length && get(this, 'config.subnetId')
+  }),
+
+  refreshDefaults: observer('authSuccess', function () {
+    if (!get(this, 'authSuccess')) {
+      return
+    }
+    const defaultImageName = get(this, 'config.imageName')
+    get(this, 'imageChoices').then(images => {
+      const defaultImageId = images.find(i => i.label === defaultImageName).value
+      set(this, 'config.imageId', defaultImageId)
+    })
+  }),
+
+  imageSelected: observer('config.imageId', function () {
+    const imageId = get(this, 'config.imageId')
+    get(this, 'imageChoices').then(images => {
+      const imageName = images.find(i => i.value === imageId).label
+      set(this, 'config.imageName', imageName)
+    })
+  }),
+
+  ubuntuSelected: observer('config.imageName', function () {
+    const imageName = get(this, 'config.imageName')
+    if (ubuntuRegex.test(imageName)) {  // default user for Ubuntu... images is `ubuntu`
+      set(this, 'config.sshUser', 'ubuntu')
+    } else {
+      set(this, 'config.sshUser', 'linux')
+    }
+  }),
+
+  volumeTypeChoices: a2f(diskTypes),
+
+})
